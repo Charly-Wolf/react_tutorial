2c89f9db87f19548db4d41cace44c5584f9116ec
Charly <cardan1990@hotmail.com>
2023-11-06 20:11:43 +0100

    Splitted data into different files

---
+++ src/books.js
@@ -0,0 +1,20 @@
+export const books = [
+  {
+    author: 'Rebecca Yarros',
+    title: 'Fourth Wing (The Empyrean, 1)',
+    img: './images/book1.jpg',
+    id: 1,
+  },
+  {
+    author: 'Rebecca Yarros',
+    title: 'Iron Flame (The Empyrean, 2)',
+    img: './images/book2.jpg',
+    id: 2,
+  },
+  {
+    author: 'Jordan Moores',
+    title: 'Interesting Facts For Curious Mind',
+    img: './images/book3.jpg',
+    id: 3,
+  },
+];
